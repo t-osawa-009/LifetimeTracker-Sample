365030fd857b9f0cdde055419adb654e130daa78
takuchantuyoshi <da87435@gmail.com>
2018-12-16 20:52:54 +0900

    ✨: 正常なmodal遷移を追加

---
+++ LifetimeTracker-Sample/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  LifetimeTracker-Sample
+//
+//  Created by Takuya Ohsawa on 2018/12/16.
+//  Copyright © 2018 Takuya Ohsawa. All rights reserved.
+//
+
+import Foundation
+
+class SecondViewController: ViewController {
+    
+    @IBAction func closeButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
