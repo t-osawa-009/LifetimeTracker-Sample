43a7fbbbb6f8e480c61c7f42768969fa27979071
takuchantuyoshi <da87435@gmail.com>
2018-12-16 18:56:21 +0900

    ✨: leakさせるためのbuttonを追加

--- LifetimeTracker-Sample/ViewController.swift
+++ LifetimeTracker-Sample/ViewController.swift
@@ -13,9 +13,21 @@ class ViewController: UIViewController {
         trackLifetime()
     }
     
+    private var viewControllers: [UIViewController] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
+    }
+    
+    @IBAction func leakButtonTapped(_ sender: Any) {
+        let vc = ViewController()
+        viewControllers.append(vc)
+    }
+    
+    @IBAction func deinitButtonTapped(_ sender: Any) {
+        guard !viewControllers.isEmpty else { return }
+        viewControllers.removeLast()
     }
 }
 
